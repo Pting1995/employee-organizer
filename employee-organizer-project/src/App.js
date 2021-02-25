489fb27e70288a388e373e2d00e812fcee287361
Peter Ting <pting1995@gmail.com>
2021-02-24 20:44:15 -0800

    added app.js functions, cleaned index.js, removed unnecessary gallery.js and added to api.js

--- employee-organizer-project/src/App.js
+++ employee-organizer-project/src/App.js
@@ -1,9 +1,48 @@
-import './App.css';
+import React, { useEffect, useState } from "react";
+import API from "./utils/API";
+import jumbotron from "./components/jumbotron";
+import searchForm from "./components/searchForm";
+import table from "./components/table";
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+  const [user, setUser] = useState({});
+  // const [userIndex, setUserIndex] = useState(0);
+
+  // When the component mounts, a call will be made to get random users.
+  useEffect(() => {
+    loadEmployees();
+  }, []);
+
+  function loadEmployees() {
+    API.getEmployeeList()
+      .then(() => {
+        API.getEmployees().then((users) => {
+          setUsers(users);
+        });
+      })
+      .catch(err => console.log(err));
+  }
+
+  // for sort by button
+  function handleBtnClick(event) {
+    if (btnName === "sortName") {
+
+    }
+  }
+
   return (
     <div>
-      
+      <jumbotron />
+      <searchForm />
+      <table
+        fullName={user.firstName + " " + user.lastName}
+        email={user.email}
+        image={user.image}
+        phone={user.phone}
+        dob={user.dob}
+      />
     </div>
   );
 }
---
+++ employee-organizer-project/src/utils/API.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+export default {
+  getEmployees: function() {
+    return new Promise((resolve, reject) => {
+      axios.get("https://randomuser.me/api/?results=15").then((res) => {
+        const users = res.data.results;
+        const results = users.map((user) => {
+          return {
+            firstname: user.name.first,
+            lastname: user.name.last,
+            email: user.email,
+            image: user.picture.large,
+            phone: user.cell,
+            dob: user.dob.date
+          };
+        });
+        resolve(results);
+      }).catch((err) => reject(err));
+    });
+  },
+  getEmployeeList: function() {
+    return new Promise((resolve) => {
+      resolve();
+    });
+  }
+};
